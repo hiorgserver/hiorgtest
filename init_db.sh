3ce517f187b58c866fa97a59d557660682d3140b
Felix Blechschmitt <fblechschmitt@hiorg-server.de>
2018-05-27 17:29:35 +0200

    Remove hiorgserver database
    Since our end-to-end tests run against the test-database, too, we do not need the `hiorgserver` database anymore.

--- init_db.sh
+++ init_db.sh
@@ -8,13 +8,9 @@ until mysqladmin ping >/dev/null 2>&1; do
   echo -n "."; sleep 0.2
 done
 
-# Create hiorgtest database and user
+# Create database and user
 mysql -e "CREATE DATABASE hiorgtest;"
 mysql -e "GRANT ALL PRIVILEGES ON hiorgtest.* TO 'hiorgserver'@'%' IDENTIFIED BY 'hiorgserver';FLUSH PRIVILEGES;"
-
-# Create hiorgserver database and user
-mysql -e "CREATE DATABASE hiorgserver;"
-mysql -e "GRANT ALL PRIVILEGES ON hiorgserver.* TO 'hiorgserver'@'%' IDENTIFIED BY 'hiorgserver';FLUSH PRIVILEGES;"
 
 # Tell the MySQL daemon to shutdown again
 service mysql stop
